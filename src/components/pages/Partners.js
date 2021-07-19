b339ba668edd1fe259c252b1e4cf6fccd70b96c5
YannickBeaucaine <yannick.beaucaine@gmail.com>
2021-07-19 19:03:49 +1000

    titles fixed

--- src/components/pages/Partners.js
+++ src/components/pages/Partners.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Cards1 from '../cards/Cards1';
+import './Partners.css'
 
 export default class Partners extends Component {
     render() {
         return (
             <div>
-                <h1>PARTNERS</h1>
+                <div className="partner-header">
+                    <div className="partner-title">
+                         <h1> MEET OUR PARTNERS</h1>
+                    </div>
+                </div>
                 <Cards1 />
             </div>
         )
