0a7deb26d179aa455eefb4651d2c22248a92a615
Chris Tzitzas <ctzitzas@gmail.com>
2021-07-19 23:42:33 +1000

    LOGIN: Added working login form.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import LoginForm from '../forms/LoginForm'
 
 export default class Home extends Component {
     render() {
         return (
             <div>
                 <h1>HOME</h1>
+                <LoginForm />
             </div>
         )
     }
