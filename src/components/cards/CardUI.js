78801cfd1014cdefee428c74f8c5ae2eb0f71f48
YannickBeaucaine <yannick.beaucaine@gmail.com>
2021-07-20 10:30:59 +1000

    created about us card

--- src/components/cards/CardUI.js
+++ src/components/cards/CardUI.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Img1 from '../images/t02greenfull.png'
+
 
 const Card1 = props => {
     return (
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import AboutCard from '../cards/AboutCard'
+import './About.css'
 
 export default class About extends Component {
     render() {
         return (
             <div>
-                <h1>ABOUT</h1>
+                <div className="partner-header">
+                    <div className="partner-title">
+                         <h1> ABOUT US</h1>
+                    </div>
+                </div>
+                <AboutCard />
             </div>
         )
     }
