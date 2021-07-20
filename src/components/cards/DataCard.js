0a2fc5f93e4a23597b031e83208f0f77917b3a12
YannickBeaucaine <yannick.beaucaine@gmail.com>
2021-07-20 16:54:47 +1000

    created sections folder

--- src/components/cards/DataCard.js
+++ src/components/cards/DataCard.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import StatImage from '../images/dataimages/trees-icon.png'
+import StatImage from '../images/statsicons/trees-icon.png'
 import './Datacard-style.css'
 
 export const DataCard = () => {
     return (
-        <div className="">
+        <div className="measure-section">
             {/* section container  */}
             <div className="container">
                 <div className="row">
@@ -30,47 +30,14 @@ export const DataCard = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="col-lg-3 cold-md-6 col-sm-12">
-                            <div className="stat-card">
-                                <div className="stat-image text-center py-4">
-                                    <img src= {StatImage} alt="Trees planted" />
-                                 <div className="card-body text-center">
-                                    <h5 className="card-title text-uppercase">Trees Planted</h5>
-                                    <p className="card-text-secondary">well hello there</p>
-                                 </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-3 cold-md-6 col-sm-12">
-                            <div className="stat-card">
-                                <div className="stat-image text-center py-4">
-                                    <img src= {StatImage} alt="Trees planted" />
-                                 <div className="card-body text-center">
-                                    <h5 className="card-title text-uppercase">Trees Planted</h5>
-                                    <p className="card-text-secondary">well hello there</p>
-                                 </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-3 cold-md-6 col-sm-12">
-                            <div className="stat-card">
-                                <div className="stat-image text-center py-4">
-                                    <img src= {StatImage} alt="Trees planted" />
-                                 <div className="card-body text-center">
-                                    <h5 className="card-title text-uppercase">Trees Planted</h5>
-                                    <p className="card-text-secondary">well hello there</p>
-                                 </div>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
-                      {/* card two               */}
-                        {/* card two               */}
-                          {/* card two               */}
-                {/* end of cards section */}
+                {/* card section end  */}
             </div>
         </div>
     )
 }
 export default DataCard;
+                   
+                     
+                    
