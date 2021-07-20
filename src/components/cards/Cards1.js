6d794089e502f0e47f1610c5385fa23864850167
YannickBeaucaine <yannick.beaucaine@gmail.com>
2021-07-20 20:17:17 +1000

    added links to partners

--- src/components/cards/Cards1.js
+++ src/components/cards/Cards1.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Card1 from './Card1';
 import img1 from '../images/partners/corner-logo.png';
 import img2 from '../images/partners/etiko-logo.png';
@@ -11,20 +12,30 @@ export default class Cards1 extends Component {
         return (
             <div className="container-fluid d-flex justify-content-center">
                     <div className="row">
-                        <div className="col-lg-4">
-                            <Card1 imgsrc={img1} partnername="The Corner Store Network"/>
+                        <div className="col-md-4 col-lg-3">
+                            <Link to={{ pathname: "https://cornerstorenetwork.org.au/"}} target="_blank">
+                                <Card1 imgsrc={img1} />
+                            </Link>
                         </div>
-                        <div className="col-lg-4">
-                            <Card1 imgsrc={img2} partnername="Etiko"/>
+                        <div className="col-md-4 col-lg-3">
+                            <Link to={{ pathname: "https://www.etiko.com.au/"}} target="_blank">
+                                <Card1 imgsrc={img2} />
+                            </Link>
                         </div>
-                        <div className="col-lg-4">
-                            <Card1 imgsrc={img3} partnername="WithOneSeed"/>
+                        <div className="col-md-4 col-lg-3">
+                            <Link to={{ pathname: "https://withoneseed.org.au"}} target="_blank">
+                                <Card1 imgsrc={img3} />
+                            </Link>
                         </div>
-                        <div className="col-lg-4">
-                            <Card1 imgsrc={img4} partnername="One Sustainable Planet"/>
+                        <div className="col-md-4 col-lg-3">
+                            <Link to={{ pathname: "https://www.onesustainableplanet.co/"}} target="_blank">
+                                <Card1 imgsrc={img4} />
+                            </Link>
                         </div>
-                        <div className="col-lg-4">
-                            <Card1 imgsrc={img5} partnername="Showroom-x"/>
+                        <div className="col-md-4 col-lg-3">
+                            <Link to={{ pathname: "https://showroom-x.com/"}} target="_blank">
+                                <Card1 imgsrc={img5} />
+                            </Link>
                         </div>
                 </div>
                 
