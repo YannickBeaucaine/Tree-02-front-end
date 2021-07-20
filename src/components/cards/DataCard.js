3c14fede250ecab598c00d2cb68d544bc64685b0
YannickBeaucaine <yannick.beaucaine@gmail.com>
2021-07-20 14:38:58 +1000

    created data card images

--- src/components/cards/DataCard.js
+++ src/components/cards/DataCard.js
@@ -1,9 +1,74 @@
 import React from 'react'
+import StatImage from '../images/dataimages/trees-icon.png'
 
 export const DataCard = () => {
     return (
-        <div>
-            <h1 className="CARD"> CARD</h1>
+        <div className="">
+            {/* section container  */}
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12 text-center data-title">
+                        <h1 className="text-uppercase title-text">Measure our impact</h1>
+                        <p className="para">
+                            Thanks to our contributors we ave achieved :
+                        </p>
+
+                    </div>
+                </div>
+                {/* cards section  */}
+                <div className="container data-cards">
+                    <div className="row">
+                        <div className="col-lg-3 cold-md-6 col-sm-12">
+                            <div className="stat">
+                                <div className="stat-image text-center py-4">
+                                    <img src= {StatImage} alt="Trees planted" />
+                                 <div className="card-body text-center">
+                                    <h5 className="card-title text-uppercase">Trees Planted</h5>
+                                    <p className="card-text-secondary">well hello there</p>
+                                 </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-3 cold-md-6 col-sm-12">
+                            <div className="stat">
+                                <div className="stat-image text-center py-4">
+                                    <img src= {StatImage} alt="Trees planted" />
+                                 <div className="card-body text-center">
+                                    <h5 className="card-title text-uppercase">Trees Planted</h5>
+                                    <p className="card-text-secondary">well hello there</p>
+                                 </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-3 cold-md-6 col-sm-12">
+                            <div className="stat">
+                                <div className="stat-image text-center py-4">
+                                    <img src= {StatImage} alt="Trees planted" />
+                                 <div className="card-body text-center">
+                                    <h5 className="card-title text-uppercase">Trees Planted</h5>
+                                    <p className="card-text-secondary">well hello there</p>
+                                 </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-3 cold-md-6 col-sm-12">
+                            <div className="stat">
+                                <div className="stat-image text-center py-4">
+                                    <img src= {StatImage} alt="Trees planted" />
+                                 <div className="card-body text-center">
+                                    <h5 className="card-title text-uppercase">Trees Planted</h5>
+                                    <p className="card-text-secondary">well hello there</p>
+                                 </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                      {/* card two               */}
+                        {/* card two               */}
+                          {/* card two               */}
+                {/* end of cards section */}
+            </div>
         </div>
     )
 }
