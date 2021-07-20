2d60388d2ade593d3cad16b7d95f0c68ed0f4c62
YannickBeaucaine <yannick.beaucaine@gmail.com>
2021-07-20 11:15:45 +1000

    finished about card

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,9 +6,9 @@ export default class About extends Component {
     render() {
         return (
             <div>
-                <div className="partner-header">
-                    <div className="partner-title">
-                         <h1> ABOUT US</h1>
+                <div className="about-header">
+                    <div className="about-title">
+                         <h1 className="about-font"> ABOUT US</h1>
                     </div>
                 </div>
                 <AboutCard />
