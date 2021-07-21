295e19cda85cb1a01abe4303aa6141e151e42737
YannickBeaucaine <yannick.beaucaine@gmail.com>
2021-07-21 14:21:15 +1000

    mystore card layout

--- src/components/pages/MyStore.js
+++ src/components/pages/MyStore.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-
+import './Header-style.css';
+import MyStoreCard from '../cards/MyStoreCard';
 
 export default class MyStore extends Component {
     render() {
         return (
             <div>
-                 <div className="header">
+                <div className="header">
                     <div className="header-title">
                          <h1 className="header-font">MY STORE</h1>
                     </div>
                 </div>
+                <MyStoreCard />
             </div>
         )
     }
