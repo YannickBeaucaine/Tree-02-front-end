43a3fda8ac64d8db619d2998e97fdd3825dbf62b
YannickBeaucaine <yannick.beaucaine@gmail.com>
2021-07-21 11:44:48 +1000

    created my trees card boxes

--- src/components/cards/TreesCard.js
+++ src/components/cards/TreesCard.js
@@ -9,13 +9,13 @@ export const TreesCard = () => {
       <div className='card-long'>
           <div className='row'>
              {/* left column */}
-             <div className='col-md-5'>
+             <div className='col-md-5 map-left'>
               {/* place map here  */}
                  {/* <img src= {TreesMap} alt="Map of trees location" className="card-img img-fluid md-6 tree-map" /> */}
               </div>
               {/* end of left column  */}
               {/* right column  */}
-              <div className='col-md-7'>
+              <div className='col-md-7 cust-stats-right'>
                   <h1 className="card-title mt-2 text-center">Thank You "username"</h1>
                     <p className="card-content text-center">
                     You have adopted "" trees-----------     
