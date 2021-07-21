2f696a0d598b2af4ece3b84144ac2f4c59c0fdb0
YannickBeaucaine <yannick.beaucaine@gmail.com>
2021-07-21 13:54:58 +1000

    routed mystore and added to navbar

---
+++ src/components/pages/MyStore.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+export default class MyStore extends Component {
+    render() {
+        return (
+            <div>
+                 <div className="header">
+                    <div className="header-title">
+                         <h1 className="header-font">MY STORE</h1>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
