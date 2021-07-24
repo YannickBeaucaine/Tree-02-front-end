b0292d40fad54cac743efc48d7a592fd05f6ae9d
YannickBeaucaine <yannick.beaucaine@gmail.com>
2021-07-24 16:05:06 +1000

    login page test pass

--- cypress/integration/loginform.spec.js
+++ cypress/integration/loginform.spec.js
@@ -4,9 +4,33 @@ describe("renders the home page",() => {
     beforeEach(() => {
         cy.visit("http://localhost:3001/")
     })
-    it("should render", ()=>{
-
+    it("render navbar", ()=>{
+        cy.get('.navbar-brand > img').click();
+        cy.get('#responsive-navbar-nav');
+        cy.get('[href="/Partners"]').click();
+        cy.get('[href="/About"]').click();
+        cy.get('[href="/About"]').click();
     })
+
+ 
+    it("login test", ()=>{
+        cy.get('.email-input');
+        cy.get('.password-input');
+        cy.get('.submit-button').click();
+    })
+    it("should render stats component", () =>{
+        cy.get('.stats-section');
+        cy.get('.col-lg-12 > .text-uppercase');
+        cy.get(':nth-child(1) > .container > .stat-card > .stat-image > img');
+        cy.get(':nth-child(2) > .container > .stat-card > .stat-image > img');
+        cy.get(':nth-child(3) > .container > .stat-card > .stat-image > img');
+        cy.get(':nth-child(4) > .container > .stat-card > .stat-image > img');
+        cy.get(':nth-child(1) > .container > .stat-card > .stat-image > .card-body > .card-text-secondary');
+    })
+  
+
+
+
 })
     
     
