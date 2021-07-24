83777f5e05e04834377c0a10d9d11f6ef52ca804
YannickBeaucaine <yannick.beaucaine@gmail.com>
2021-07-24 14:16:28 +1000

    loginform rendering testing

---
+++ cypress/integration/loginform.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe("renders the home page",() => {
+    beforeEach(() => {
+        cy.visit("http://localhost:3001/")
+    })
+    it("should render", ()=>{
+
+    })
+})
+    
+    
+
+
+ 
