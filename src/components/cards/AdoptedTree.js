22c5e4d564eba67f20cb475958e3027bbb899e1a
YannickBeaucaine <yannick.beaucaine@gmail.com>
2021-07-25 16:56:56 +1000

    added cards to be filled

--- src/components/cards/AdoptedTree.js
+++ src/components/cards/AdoptedTree.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from 'react';
+import './AdoptedTree-style.css';
 
 export const AdoptedTree = () => {
     return (
-        <div>
-            Hello
+        <div className='card-container adopted-tree-section'>
+        <div className='card-long'>
+            <div className='row'>
+               {/* left column */}
+               <div className='col-md-5 map-left adopted-tree-map-responsive'>
+                {/* place map here  */}
+                    MAP
+                </div>
+                {/* end of left column  */}
+                {/* right column  */}
+                <div className='col-md-7 adopted-tree-right'>
+                    <h1 className="card-title mt-2 text-center adopted-header">TEST</h1>
+                   
+                      <p className="card-content text-center adopted-text">
+                            TEST
+                      </p>
+                </div>
+                {/* end of right column */}
+            </div>
         </div>
+    </div>
     )
 }
 
