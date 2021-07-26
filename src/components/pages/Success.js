09cc690abe7a8a847ad2c21004e22e5e269d9341
YannickBeaucaine <yannick.beaucaine@gmail.com>
2021-07-26 17:21:18 +1000

    added route for loading page

--- src/components/pages/Success.js
+++ src/components/pages/Success.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './Header-style.css';
+import LoadingSpinner from '../cards/LoadingSpinner'
+import './Success-style.css';
 
 
 
@@ -7,13 +9,16 @@ export default class SuccessPage extends Component {
     render() {
         return (
             <div>
-                <div className="header">
-                    <div className="header-title">
-                         <h1 className="header-font">THANK YOU !</h1>
-                    </div>
+            <div className="header">
+                <div className="header-title">
+                     <h1 className="header-font"> THANK YOU!</h1>
                 </div>
-              
             </div>
+            <LoadingSpinner />
+            <div>
+             
+            </div>
+        </div>
         )
     }
 }
