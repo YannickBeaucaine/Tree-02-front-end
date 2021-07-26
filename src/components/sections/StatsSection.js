f50a985b1a1c86224f45b6a78db1e677eec735ad
YannickBeaucaine <yannick.beaucaine@gmail.com>
2021-07-26 14:26:04 +1000

    homepage styling update

--- src/components/sections/StatsSection.js
+++ src/components/sections/StatsSection.js
@@ -18,38 +18,40 @@ const StatsSection = ({data}) => {
             return null;
         }
         return (
-            <div className="stats-section">
-         
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-12 text-center data-title">
-                        <h1 className="text-uppercase title-text">Measure our impact</h1>
-                        <p className="para">
-                            Thanks to our contributors we have achieved
-                        </p>
+            <div className="color-container">
+                    <div className="stats-section">
+                
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-12 text-center data-title">
+                                <h1 className="text-uppercase title-text">Measure our impact</h1>
+                                <p className="para">
+                                    Thanks to our contributors we have achieved
+                                </p>
 
+                            </div>
+                        </div>
+                        {/* cards section  */}
+                            <div className="row">
+                                {/* card 1  */}
+                                <div className="col-lg-3 cold-md-6 col-sm-12 px-1">
+                                    <DataCard staticon={TreeImg} statname="Trees Planted" statnumber={data.totalTreeCount} staticonalt="tree icon"/>
+                                </div>
+                                {/* card 2  */}
+                                <div className="col-lg-3 cold-md-6 col-sm-12 px-1">
+                                    <DataCard staticon={FarmImg} statname="Farmers" statnumber={data.totalFarmers} staticonalt="farmer icon"/>
+                                </div>
+                                {/* card 3  */}
+                                <div className="col-lg-3 cold-md-6 col-sm-12 px-1">
+                                    <DataCard staticon={MoneyImg} statname="Farmers earnings" statnumber={data.totalFarmerEarnings} staticonalt="farmer iscon icon"/>
+                                </div>
+                                {/* card 4  */}
+                                <div className="col-lg-3 cold-md-6 col-sm-12 px-1">
+                                    <DataCard staticon={CloudImg} statname="CO2 Asborbed" statnumber={data.tonnesOfCarbonSequestered} staticonalt="Co2 absorbtion icon"/>
+                                </div>
+                            </div>
+                        {/* card section end  */}
                     </div>
-                </div>
-                {/* cards section  */}
-                    <div className="row">
-                        {/* card 1  */}
-                        <div className="col-lg-3 cold-md-6 col-sm-12">
-                             <DataCard staticon={TreeImg} statname="Trees Planted" statnumber={data.totalTreeCount} staticonalt="tree icon"/>
-                        </div>
-                        {/* card 2  */}
-                        <div className="col-lg-3 cold-md-6 col-sm-12">
-                             <DataCard staticon={FarmImg} statname="Farmers" statnumber={data.totalFarmers} staticonalt="farmer icon"/>
-                        </div>
-                        {/* card 3  */}
-                        <div className="col-lg-3 cold-md-6 col-sm-12">
-                             <DataCard staticon={MoneyImg} statname="Farmers earnings" statnumber={data.totalFarmerEarnings} staticonalt="farmer iscon icon"/>
-                        </div>
-                        {/* card 4  */}
-                        <div className="col-lg-3 cold-md-6 col-sm-12">
-                             <DataCard staticon={CloudImg} statname="CO2 Asborbed" statnumber={data.tonnesOfCarbonSequestered} staticonalt="Co2 absorbtion icon"/>
-                        </div>
-                    </div>
-                {/* card section end  */}
             </div>
         </div>
         )
