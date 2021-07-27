6c6e2b1841aa6a64ac4193aa8cf39eab7cc48163
YannickBeaucaine <yannick.beaucaine@gmail.com>
2021-07-28 02:23:52 +1000

    fixed mytrees card

--- src/components/cards/TreesCard.js
+++ src/components/cards/TreesCard.js
@@ -6,22 +6,22 @@ import MyTreesMap from '../maps/MyTreesMap';
 export const TreesCard = () => {
     return (
 
-      <div className='card-container'>
-      <div className='card-long'>
+      <div className='card-container whole-card'>
+      <div className='card-long my-trees'>
           <div className='row'>
              {/* left column */}
-             <div className='col-md-5 map-left tree-map-responsive'>
+             <div className='col-lg-7 map-left tree-map-responsive px-0' style={{ width: '22rem' }} >
               {/* place map here  */}
               <MyTreesMap />
                  {/* <img src= {TreesMap} alt="Map of trees location" className="card-img img-fluid md-6 tree-map" /> */}
               </div>
               {/* end of left column  */}
               {/* right column  */}
-              <div className='col-md-7 cust-stats-right'>
-                  <h1 className="card-title mt-2 text-center">Thanks {sessionStorage.getItem('name')}!</h1>
-                    <p className="card-content text-center">
+              <div className='col-lg-5 '>
+                  <h1 className="card-title  text-center cust-name " style={{ width: '20rem' }}>Thanks <br/>{sessionStorage.getItem('name')}!</h1>
+                    <h4 className="card-content text-center" style={{ width: '20rem' }}>
                     You have adopted {sessionStorage.getItem('name').length} trees    
-                    </p>
+                    </h4>
               </div>
               {/* end of right column */}
           </div>
